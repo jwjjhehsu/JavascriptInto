2967ecc0fb98623b7f0ebe55b4bfc6d89dfed55e
priyarajmohnaty01 <119916872+jwjjhehsu@users.noreply.github.com>
2024-08-13 16:41:37 +0000

    datatype completed

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,30 @@
+"use strict";// treat all js code as newer version
+
+
+let name = "hitesh";
+let age = 19;
+let isLoggeediN = true;
+
+
+
+// data types in js
+// Number 
+// String = " "
+// boolean = "true" or  "false"
+// null=> standalone type
+// undefined
+// symbol=.unique values
+
+
+// object
+
+let aged = 55;
+console.log(typeof "hitesh");
+console.log(typeof aged);
+
+console.log(typeof undefined);
+console.log(typeof null);
+
+
+
+
