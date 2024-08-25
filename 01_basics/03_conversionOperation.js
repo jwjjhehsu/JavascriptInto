70aa14f383370fa3eaa1a4dd92d319ee92ce0030
priyarajmohnaty01 <119916872+jwjjhehsu@users.noreply.github.com>
2024-08-25 06:01:12 +0000

    ConversionoperationCompleted

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -37,12 +37,22 @@ console.log(typeof valueInNumber); // number
 console.log(valueInNumber); // 1  and for "false" it will return as 0
 
 // Similarly for String 
-score = "hello"
-valueInNumber = Number(score);
 
-console.log(typeof valueInNumber); //number
-console.log("string converted to "+ valueInNumber); //1
+console.log("/////////////////////test///////////////");
 
+
+let check = "Hello";
+let valueinnmber = Number(check);
+console.log(valueinnmber);//NaN
+console.log(typeof valueinnmber);//number
+
+ check = "";
+valueinnmber = Number(check);
+console.log(valueinnmber);// 0 
+console.log(typeof valueinnmber);//number
+
+
+console.log("-----------------Boolean to others Conversion-----------------------")
 
 let isLoggedIn = 1
 
@@ -74,13 +84,77 @@ console.log(booleanIsLoggedIn); //true
 console.log(typeof booleanIsLoggedIn); //boolean
 
 
-
+console.log("=-------------------------------------=")
 // Similarly we can convert a number to string also
 
 let somenumber = 444;
 let stringNumber = String(somenumber);
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+
+
+//  ********************OPERATIONS IN JAVASCRIPT **************************************//
+let value = 3;
+let negValue = -(value);
+console.log(negValue);
+// Arithematic ooperations
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.group(2/2);
+console.log(10%2);
+console.log(2**4);  //power
+
+
+let str1 = "hello";
+let str2 = "priyaraj";
+console.log(str1+ str2);
+
+
+// Special Cases
+console.log(2+ "4");24
+console.log("2" + 4);24
+console.log(2+2+ "7");47
+console.log("2"+ 4+4);244
+
+
+
+
+
+
+console.log("+++++++++++++++++++++++some special condition .+++++++++++++++++++++++++++++++++ ")
+
+console.log(+true) //1;
+// console.log(true+) //error
+console.log("error");
+
+console.log("=================================================================");
+
+
+
+
+// other assignment operators
+let num1,num2,num3;
+
+num1 = num2 = num3 = 3+2;
+console.log(num1);
+console.log(num2);
+console.log(num3);
+
+
+// Increment and Decrement operators
+let gameCount = 100;
+gameCount++; //post increment -> first use then increment
+gameCount--; //post decrement -> first use then decrement
+
+++gameCount; //pre increment -> first increement then use
+--gameCount; //pre-decrement -> first decrement and the use
+
+// some more operations ;
+console.log(gameCount); //current value
+let result = gameCount++ + ++gameCount + 7 + --gameCount - gameCount--;
+console.log(result);//209
 
 
 
